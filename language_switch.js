b29aa62dd508b1556676171d9ab90fb887e7282e
AnnonyWOLFRODA <125645180+AnnonyWOLFRODA@users.noreply.github.com>
2023-11-27 22:55:23 +0100

    main files

--- language_switch.js
+++ language_switch.js
@@ -4,18 +4,16 @@ function switchLanguage(lang) {
         document.getElementById('construction-subtitle-en').style.display = 'none';
         document.getElementById('construction-text-fr').style.display = 'block';
         document.getElementById('construction-text-en').style.display = 'none';
-        document.getElementById('discord-link-fr').style.display = 'block';
-        document.getElementById('discord-link-en').style.display = 'none';
         document.querySelector('.language-switch a[onclick="switchLanguage(\'fr\')"]').classList.add('active');
         document.querySelector('.language-switch a[onclick="switchLanguage(\'en\')"]').classList.remove('active');
+        window.location.href = '/fr';
     } else if (lang === 'en') {
         document.getElementById('construction-subtitle-fr').style.display = 'none';
         document.getElementById('construction-subtitle-en').style.display = 'block';
         document.getElementById('construction-text-fr').style.display = 'none';
         document.getElementById('construction-text-en').style.display = 'block';
-        document.getElementById('discord-link-fr').style.display = 'none';
-        document.getElementById('discord-link-en').style.display = 'block';
         document.querySelector('.language-switch a[onclick="switchLanguage(\'fr\')"]').classList.remove('active');
         document.querySelector('.language-switch a[onclick="switchLanguage(\'en\')"]').classList.add('active');
+        window.location.href = '/en';
     }
 }
